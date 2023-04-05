e00fd0207d4133644da5b7f388b5030a44db592a
Bryan Cohen <82907967+bryantqr@users.noreply.github.com>
2023-04-05 13:16:18 -0400

    adding tests to job validator class

---
+++ mirrulations-validation/tests/test_job_validator.py
@@ -0,0 +1,7 @@
+from mirrval.job_validator import Validator
+from mirrval.job_validator import write_unfound_jobs
+from mirrmock.mock_data_storage import MockDataStorage
+from mirrcore.regulations_api import RegulationsAPI
+
+def test_download(self):
+    pass
