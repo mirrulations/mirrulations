0de0a0829817ca77e1ece7a803d3a028e41f8340
Bryan Cohen <82907967+bryantqr@users.noreply.github.com>
2023-04-21 14:22:31 -0400

    adding bucket name

--- mirrulations-core/src/mirrcore/bucket_size.py
+++ mirrulations-core/src/mirrcore/bucket_size.py
@@ -1,22 +1,22 @@
 import boto3
 from datetime import datetime, timedelta
 
-
 class BucketSize:
 
-    def get_bucket_size(bucket_name: str, region: str):
-        now = datetime.datetime.now()
+    def get_bucket_size():
+        bucket_name = 'mirrulations'
+        now = datetime.utcnow()
+        start_time = now - timedelta(days=1)
+        end_time = now
 
-        bucket_name = ...
-
-        cloudwatch = boto3.client("cloudwatch", region_name=region)
+        cloudwatch = boto3.client("cloudwatch")
         result = cloudwatch.get_metric_statistics(
             Namespace="AWS/S3",
             Dimensions=[{"Name": "BucketName", "Value": bucket_name},
                         {"Name": "StorageType", "Value": "StandardStorage"}],
             MetricName="BucketSizeBytes",
-            StartTime=datetime.now() - timedelta(1),
-            EndTime=datetime.now(),
+            StartTime=start_time,
+            EndTime=end_time,
             Period=86400,
             Statistics=['Average'],
             Unit='Bytes'
