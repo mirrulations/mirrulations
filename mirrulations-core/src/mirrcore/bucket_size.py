8d445c513b6e68d84d5ff392f33af61c9e5c3ea7
Bryan Cohen <82907967+bryantqr@users.noreply.github.com>
2023-04-21 14:09:05 -0400

    removing static errors

--- mirrulations-core/src/mirrcore/bucket_size.py
+++ mirrulations-core/src/mirrcore/bucket_size.py
@@ -1,18 +1,23 @@
 import boto3
-import datetime
+from datetime import datetime, timedelta
 
 class BucketSize:
 
     def get_bucket_size(bucket_name: str, region: str):
+        now = datetime.datetime.now()
+
+        bucket_name = ...
+
         cloudwatch = boto3.client("cloudwatch", region_name=region)
         result = cloudwatch.get_metric_statistics(
             Namespace="AWS/S3",
             Dimensions=[{"Name": "BucketName", "Value": bucket_name},
                         {"Name": "StorageType", "Value": "StandardStorage"}],
             MetricName="BucketSizeBytes",
-            StartTime=datetime.now() - timedelta(2),
+            StartTime=datetime.now() - timedelta(1),
             EndTime=datetime.now(),
             Period=86400,
             Statistics=['Average'],
+            Unit='Bytes'
         )
         return result["Datapoints"][0]["Average"]
