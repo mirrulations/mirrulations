6f8bad939df52f5632e124e13854abbaae9ad9cf
Ben Coleman <colemanb@moravian.edu>
2023-05-23 14:56:10 -0400

    Refactor to pytest and mock out time.sleep

--- mirrulations-core/tests/test_data_counts.py
+++ mirrulations-core/tests/test_data_counts.py
@@ -1,44 +1,33 @@
-import unittest
-from unittest.mock import MagicMock, patch
+from unittest.mock import MagicMock
 from mirrcore.data_counts import DataCounts
 
 
-class TestDataCounts(unittest.TestCase):
+def test_get_counts(mocker):
+    api_key = "test"
+    dockets_response = MagicMock()
+    dockets_response.json.return_value = {"meta": {"totalElements": 500}}
+    documents_response = MagicMock()
+    documents_response.json.return_value = {
+        "meta": {"totalElements": 1000}}
+    comments_response = MagicMock()
+    comments_response.json.return_value = {"meta": {"totalElements": 2500}}
 
-    # Test that each count function returns the expected values
-    @patch('requests.get')
-    def test_get_counts(self, mock_api_request):
-        """Tests that each count function returns the expected value"""
-        # Set up mock data and objects
-        api_key = "test"
-        dockets_response = MagicMock()
-        dockets_response.json.return_value = {"meta": {"totalElements": 500}}
-        documents_response = MagicMock()
-        documents_response.json.return_value = {
-            "meta": {"totalElements": 1000}}
-        comments_response = MagicMock()
-        comments_response.json.return_value = {"meta": {"totalElements": 2500}}
-        mock_api_request.side_effect = [
-            dockets_response,
-            documents_response,
-            comments_response
-            ]
+    mocker.patch('time.sleep')
+    mocker.patch('requests.get', side_effect=[
+        dockets_response,
+        documents_response,
+        comments_response
+        ])
 
-        # Call the function we want to test
-        counts = DataCounts(api_key).get_counts()
+    counts = DataCounts(api_key).get_counts()
 
-        # Make assertions about the function's behavior
-        self.assertEqual(counts, [500, 1000, 2500])  # Expected results
+    assert counts == [500, 1000, 2500]
 
-    def test__get_total_elements(self):
-        """Tests that __get_total_elements correctly extracts
-          the 'totalElements' value from a response"""
-        # Set up mock data and objects
-        response = {"meta": {"totalElements": 123}}
 
-        # Call the function we want to test
-        total_elements = DataCounts(  # pylint: disable=W0212
-            api_key="test")._DataCounts__get_total_elements(response)
+def test__get_total_elements():
+    response = {"meta": {"totalElements": 123}}
 
-        # Make assertions about the function's behavior
-        self.assertEqual(total_elements, 123)  # Expected result
+    total_elements = DataCounts(  # pylint: disable=W0212
+        api_key="test")._DataCounts__get_total_elements(response)
+
+    assert total_elements == 123
