9f14daf0e0aa044e62b3d44842c1a53c1081c633
Braden <kirkpatrickb02@moravian.edu>
2026-04-17 15:14:38 -0400

    add startlate info and all timestamps

--- startlate.sh
+++ startlate.sh
@@ -1,3 +1,6 @@
+# run with
+# ./startlate.sh day-month-year hour:minute:second
+# ./startlate.sh 2026-04-11 11:46:42
 if [[ -z "$1" ]]; then
     echo "Error: timestamp argument is required"
     echo "Usage: $0 <timestamp>"
@@ -7,3 +10,7 @@ docker compose build
 docker compose up -d
 redis-cli set dockets_last_timestamp "$1 $2"
 redis-cli get dockets_last_timestamp
+redis-cli set documents_last_timestamp "$1 $2"
+redis-cli get documents_last_timestamp
+redis-cli set comments_last_timestamp "$1 $2"
+redis-cli get comments_last_timestamp
