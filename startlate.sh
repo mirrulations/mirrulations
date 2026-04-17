0c1767cc12622b759e79760b89f7e86442d92753
Braden <kirkpatrickb02@moravian.edu>
2026-04-17 15:43:34 -0400

    make better

--- startlate.sh
+++ startlate.sh
@@ -7,10 +7,10 @@ if [[ -z "$1" ]]; then
     exit 1
 fi
 docker compose build
-docker compose up -d
 redis-cli set dockets_last_timestamp "$1 $2"
 redis-cli get dockets_last_timestamp
 redis-cli set documents_last_timestamp "$1 $2"
 redis-cli get documents_last_timestamp
 redis-cli set comments_last_timestamp "$1 $2"
 redis-cli get comments_last_timestamp
+docker compose up -d nginx redis work_generator dashboard client1
